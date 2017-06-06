3c375fd752c3c68de6d98cf0d279fec6d20f75bb
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2017-06-06 19:01:09 +0700

    Refactor urls to exclude namespace.

--- urls.py
+++ urls.py
@@ -1,10 +1,8 @@
-from django.views.generic.base import TemplateView
-
-from geosafe.views import metasearch
-
 __author__ = 'ismailsunni'
+
 from django.conf.urls import patterns, url
 
+from geosafe.views import metasearch
 from geosafe.views.analysis import (
     AnalysisListView,
     AnalysisCreateView,
@@ -17,42 +15,42 @@ from geosafe.views.analysis import (
 urlpatterns = patterns(
     '',
     url(
-        r'^geosafe/analysis/create(?:/(?P<pk>\d*))?$',
+        r'^analysis/create(?:/(?P<pk>\d*))?$',
         AnalysisCreateView.as_view(),
         name='analysis-create'
     ),
     url(
-        r'^geosafe/analysis/(?:/user/(?P<user>\d*))?$',
+        r'^analysis/(?:/user/(?P<user>\d*))?$',
         AnalysisListView.as_view(),
         name='analysis-list'
     ),
     url(
-        r'^geosafe/analysis/impact-function-filter$',
+        r'^analysis/impact-function-filter$',
         impact_function_filter,
         name='impact-function-filter'
     ),
     url(
-        r'^geosafe/analysis/(?P<pk>\d+)$',
+        r'^analysis/(?P<pk>\d+)$',
         AnalysisDetailView.as_view(),
         name='analysis-detail'
     ),
     url(
-        r'^geosafe/analysis/layer-tiles$',
+        r'^analysis/layer-tiles$',
         layer_tiles,
         name='layer-tiles'
     ),
     url(
-        r'^geosafe/analysis/layer-metadata/(?P<layer_id>\d+)',
+        r'^analysis/layer-metadata/(?P<layer_id>\d+)',
         layer_metadata,
         name='layer-metadata'
     ),
     url(
-        r'^geosafe/analysis/layer-archive/(?P<layer_id>\d+)',
+        r'^analysis/layer-archive/(?P<layer_id>\d+)',
         layer_archive,
         name='layer-archive'
     ),
     url(
-        r'^geosafe/analysis/layer-list/'
+        r'^analysis/layer-list/'
         r'(?P<layer_purpose>(hazard|exposure|aggregation|impact))'
         r'(?:/(?P<layer_category>\w*))?'
         r'(?:/(?P<bbox>[,.\d-]*))?',
@@ -60,38 +58,38 @@ urlpatterns = patterns(
         name='layer-list'
     ),
     url(
-        r'^geosafe/analysis/layer-panel'
+        r'^analysis/layer-panel'
         r'(?:/(?P<bbox>[\[\],.\d-]*))?',
         layer_panel,
         name='layer-panel'
     ),
     url(
-        r'^geosafe/analysis/rerun/'
+        r'^analysis/rerun/'
         r'(?P<analysis_id>\d+)',
         rerun_analysis,
         name='rerun-analysis'
     ),
     url(
-        r'^geosafe/analysis/check/'
+        r'^analysis/check/'
         r'(?P<analysis_id>\d+)',
         analysis_json,
         name='check-analysis'
     ),
     url(
-        r'^geosafe/analysis/toggle-saved/'
+        r'^analysis/toggle-saved/'
         r'(?P<analysis_id>[-\d]+)',
         toggle_analysis_saved,
         name='toggle-analysis-saved'
     ),
     url(
-        r'^geosafe/analysis/report/'
+        r'^analysis/report/'
         r'(?P<analysis_id>\d+)/'
         r'(?P<data_type>(map|table|reports|all))',
         download_report,
         name='download-report'
     ),
     url(
-        r'^geosafe/analysis/summary/'
+        r'^analysis/summary/'
         r'(?P<impact_id>[-\d]+)/',
         analysis_summary,
         name='analysis-summary'
@@ -99,33 +97,33 @@ urlpatterns = patterns(
 
     # Metasearch
     url(
-        r'^geosafe/metasearch/$',
+        r'^metasearch/$',
         metasearch.index,
         name='metasearch'
     ),
     url(
-        r'^geosafe/metasearch/csw_ajax$',
+        r'^metasearch/csw_ajax$',
         metasearch.csw_ajax,
         name='metasearch_csw_ajax'
     ),
     url(
-        r'^geosafe/metasearch/add_layer$',
+        r'^metasearch/add_layer$',
         metasearch.add_layer,
         name='metasearch_add_layer'
     ),
     url(
-        r'^geosafe/metasearch/add_layer_dialog',
+        r'^metasearch/add_layer_dialog',
         metasearch.show_add_layer_dialog,
         name='metasearch_add_layer_dialog'
     ),
     url(
-        r'^geosafe/metasearch/show_metadata',
+        r'^metasearch/show_metadata',
         metasearch.show_metadata,
         name='metasearch_show_metadata'
     ),
 
     url(
-        r'^geosafe/metasearch/wfs_proxy',
+        r'^metasearch/wfs_proxy',
         metasearch.wfs_proxy,
         name='metasearch_wfs_proxy'
     ),
