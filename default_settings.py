58225a74644941437c57d441cd9a748c5743054c
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2017-09-21 12:10:08 +0700

    Fix getting the settings as the correct object

--- default_settings.py
+++ default_settings.py
@@ -58,13 +58,13 @@ GEONODE_BASE_URL = 'http://localhost:8000/'
 
 # Analysis Run Time Limit (in seconds)
 # Task will exit if exceeded this hard limit
-INASAFE_ANALYSIS_RUN_TIME_LIMIT = os.environ.get(
-    'INASAFE_ANALYSIS_RUN_TIME_LIMIT', 600)
+INASAFE_ANALYSIS_RUN_TIME_LIMIT = literal_eval(os.environ.get(
+    'INASAFE_ANALYSIS_RUN_TIME_LIMIT', '600'))
 
 
 # Analysis area limit (in meter squares)
 # Create analysis will display warning if analysis extent
 # exceeded this limit. User will be able to continue analysis
 # with warning that analysis will might take a long time.
-INASAFE_ANALYSIS_AREA_LIMIT = os.environ.get(
-    'INASAFE_ANALYSIS_AREA_LIMIT', 1000000000)
+INASAFE_ANALYSIS_AREA_LIMIT = literal_eval(os.environ.get(
+    'INASAFE_ANALYSIS_AREA_LIMIT', '1000000000'))
