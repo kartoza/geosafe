77cb0dfd459c282a59d613f43f482976dc0e5d82
myarjunar <myarjunar@gmail.com>
2018-09-12 11:30:15 +0700

    use hazard aggregation summary for continuous raster exposure

--- tasks/analysis.py
+++ tasks/analysis.py
@@ -211,7 +211,11 @@ def set_layer_purpose(keywords, layer_id):
     layer = Layer.objects.get(id=layer_id)
     metadata, created = Metadata.objects.get_or_create(layer=layer)
 
-    metadata.layer_purpose = keywords.get('layer_purpose', None)
+    layer_purpose = keywords.get('layer_purpose', None)
+    metadata.layer_purpose = (
+        layer_purpose if (
+            layer_purpose != 'hazard_aggregation_summary') else (
+            'impact_analysis'))
     metadata.category = keywords.get(metadata.layer_purpose, None)
     metadata.save()
 
@@ -298,7 +302,9 @@ def process_impact_result(self, impact_result, analysis_id):
     analysis.save()
 
     if impact_result['status'] == RESULT_SUCCESS:
-        impact_url = impact_result['output']['impact_analysis']
+        impact_url = (
+            impact_result['output'].get('impact_analysis') or
+            impact_result['output'].get('hazard_aggregation_summary'))
 
         # generate report when analysis has ran successfully
         async = generate_report.delay(impact_url)
